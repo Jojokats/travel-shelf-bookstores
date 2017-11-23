7d196d063ea93e0d2ae3b26996e0871c2dcb4106
Joanna <morningstarjk@hotmail.com>
2017-11-22 21:40:17 -0500

    Initial commit

---
+++ accessing-array-values.js
@@ -0,0 +1,4 @@
+var food = ['apple', 'pizza', 'pear'];
+
+// Access the 2nd value or this array
+console.log(food[1]);
---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function() {
+
+  // Cache the window object
+  var $window = $(window);
+
+  // Parallax background effect
+  $('section[data-type="background"]').each(function() {
+
+    var $bgobj = $(this); // assigning the object
+
+    $(window).scroll(function() {
+            var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+
+            // Put together our final background position
+            var coords = '50% '+ yPos + 'px';
+
+            // Move the background
+            $bgobj.css({ backgroundPosition: coords });
+    }); // end window scroll
+    
+  });
+});
